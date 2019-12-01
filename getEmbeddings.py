b2b2e72130908709ac4cefc31944b3bae9439c07
Wen Zhou <wzhou14fall@gmail.com>
2019-12-01 12:38:59 -0800

    getEmbedding

---
+++ getEmbeddings.py
@@ -0,0 +1,14 @@
+import pickle
+import numpy as np
+import torch
+
+entityInput = open("data/outputData/entity2vec50.pickle", "rb")
+relationInput = open("data/outputData/relation2vec.pickle", "rb")
+
+tmpEntityEmbedding = pickle.load(entityInput)
+tmpRelationEmbedding = pickle.load(relationInput)
+entityInput.close()
+relationInput.close()
+
+np.savetxt('entity_emb_50.txt', tmpEntityEmbedding.numpy())
+np.savetxt('relation_emb_50.txt', tmpRelationEmbedding.numpy())
